77032a4f05c61437884755c1d019ec21df38d6ab
Ahmad Rafique <imac@iMacs-iMac.local>
2022-02-22 10:21:53 +0500

    consultant state

--- lib/src/modules/user/all_consultants/state.dart
+++ lib/src/modules/user/all_consultants/state.dart
@@ -2,11 +2,15 @@ import 'dart:ui';
 
 import 'package:consultant_product/src/utils/colors.dart';
 import 'package:consultant_product/src/utils/constants.dart';
+import 'package:flutter/material.dart';
 import 'package:resize/resize.dart';
 
 class AllConsultantsState {
   TextStyle? headingTextStyle;
   TextStyle? subHeadingTextStyle;
+  TextStyle? tabTextStyle;
+  TextStyle? consultantNameTextStyle;
+  TextStyle? consultantCategoryTextStyle;
 
   AllConsultantsState() {
     ///Initialize variables
@@ -16,6 +20,24 @@ class AllConsultantsState {
       fontSize: 28.sp,
       color: customLightThemeColor,
     );
-    subHeadingTextStyle = TextStyle();
+    subHeadingTextStyle = TextStyle(
+        fontFamily: SarabunFontFamily.medium,
+        fontSize: 12.sp,
+        color: Colors.white);
+    tabTextStyle = TextStyle(
+      fontFamily: SarabunFontFamily.semiBold,
+      fontSize: 14.sp,
+      color: customLightThemeColor,
+    );
+    consultantNameTextStyle = TextStyle(
+      fontFamily: SarabunFontFamily.extraBold,
+      fontSize: 14.sp,
+      color: customTextBlackColor,
+    );
+    consultantCategoryTextStyle = TextStyle(
+      fontFamily: SarabunFontFamily.light,
+      fontSize: 11.sp,
+      color: customTextBlackColor,
+    );
   }
 }
